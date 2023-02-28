4e7f4231772bc27160dddb30c4b5ae255ae9b9a2
Pipo <36411075+PipoSurGitHub@users.noreply.github.com>
2023-02-28 12:19:06 +0100

    Change TOKEN

--- src/main/java/eu/cubix/mc/bot/discord/Main.java
+++ src/main/java/eu/cubix/mc/bot/discord/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) throws LoginException {
         jda = JDABuilder
-                .createDefault("NTE1NTQ5MDI0Mjk2NjMyMzMy.W_gccQ.c4yfia_GZSQrHiYGjss0oZwB-GE")
+                .createDefault(TOKEN)
                 .addEventListeners(new MemberEvent())
                 .setActivity(Activity.playing("CubixMC"))
                 .build();
